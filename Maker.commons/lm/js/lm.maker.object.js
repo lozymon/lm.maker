eaf671834eb263ff42a184c4db50ed95ded39487
Kim Andre Furevikstrand <lozymon@gmail.com>
2016-11-15 22:23:26 -0300

    Updated code documentation

--- Maker.commons/lm/js/lm.maker.object.js
+++ Maker.commons/lm/js/lm.maker.object.js
@@ -5,23 +5,55 @@
     var maker_object = {
         version: '0.1',
         author: 'Kim Andre Furevikstrand',
-        email: 'lozymon@gmail.com',
-        event: {},
+        email: 'lozymon@gmail.com'
     }
 
+    /**
+     * Creating new Object
+     *
+     * @return Object
+     */
     maker_object.createObject = function() {
         return new Object;
     }
 
+    /**
+     * Set a properties and it's value
+     *
+     * @param  Object object   Object thets resiving property and it's value
+     * @param  String property Name of the property
+     * @param  Object value    Value to set to the property
+     * @return Obejct
+     */
     maker_object.setObjectProperty = function( object, property, value ) {
         object[ property ] = value;
         return object;
     }
 
+    /**
+     * Gets a value from a given property
+     * @param  Object object   Object
+     * @param  String property Name of the property
+     * @return Object
+     */
     maker_object.getObjectProperty = function( object, property ) {
         return object[ property ];
     }
 
+    /**
+     * Convert a array list to a object
+     *
+     * ex:
+     *   [['property1', 'value1'], ['property2', 'value2']]
+     * to:
+     *   {
+     *     property1: 'value1',
+     *     property2: 'value2'
+     *   }
+     *
+     * @param  Array arrayList array
+     * @return Object
+     */
     maker_object.arrayListToObject = function( arrayList ) {
         var obj = new Object;
 
