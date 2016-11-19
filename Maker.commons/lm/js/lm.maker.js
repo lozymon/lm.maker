04e83973216902c70460712403d05d55a05a5178
Kim Andre Furevikstrand <lozymon@gmail.com>
2016-11-19 15:01:09 -0300

    adding functions  to import third party objects
    adding functions  to import third party objects
    used to import objects global in maker

--- Maker.commons/lm/js/lm.maker.js
+++ Maker.commons/lm/js/lm.maker.js
@@ -7,6 +7,7 @@
         author: 'Kim Andre Furevikstrand',
         email: 'lozymon@gmail.com',
         isDebugging: false,
+        tpo: []
     }
 
     /**
@@ -63,7 +64,89 @@
         return !!obj && Array === obj.constructor;
     }
 
+    /**
+     * add Javascript tag to the element
+     *
+     * @param  Element elemnt
+     * @param  Path    path   path to the javascript
+     * @return void
+     */
+    Lozymon.addJavaScript function ( elemnt, path )
+    {
+        // load as tag
+        if ( element ) {
+            var script  = document.createElement('script');
+            script.src  = path;
+            script.type = "text/javascript";
+            elemnt.appendChild(script);
+        }
+        // load with webrun
+        else {
+            webrun.include( path );
+        }
+    }
+
+    /**
+     * add StyleSheet tag to the element
+     * if element not is informed then use webrun include to load javascript object
+     *
+     * @param  Element element
+     * @param  Path    path   path to the stylesheet
+     * @return void
+     */
+    Lozymon.addStyleSheet = function ( element, path )
+    {
+        var link  = document.createElement('link');
+        link.href = path;
+        link.type = "text/css";
+        link.rel  = "stylesheet";
+        element.appendChild( link );
+    }
+
+    /**
+     * Load tpo files
+     *
+     * @return void
+     */
+    Lozymon.loadTPO = function()
+    {
+        // load tpo file
+        webrun.include('lm/tpo.json.js');
+        // sort list
+        items.sort(function (a, b) {
+          if (a.sort > b.sort) { return 1; }
+          if (a.sort < b.sort) { return -1;}
+          return 0;
+        });
+        // get body element
+        var v_bodyArray = document.getElementsByTagName("body");
+        // if body exists
+        if (v_bodyArray.length) {
+            // get first body element
+            var v_body = v_bodyArray[0];
+            // add all tpo to the body
+            for (var i = 0; i < Lozymon.tpo.length ; i++) {
+                // load object
+                var tpo = Lozymon.tpo[i];
+                // create path with version
+                var path = tpo.path + '?lm_v=' + tpo.version;
+                // if type is javascript
+                if (tpo.type == 'J') {
+                    var element = ( tpo.useWebrunInclude == true ) ? null : b_body;
+                    this.addJavaScript( element, path );
+                }
+                // if type is StyleSheet
+                else if (tpo.typo == 'S') {
+                    this.addStyleSheet( v_body, path );
+                }
+            }
+        }
+    }
+
     window.lm = Lozymon;
+
+    // load objects
+    Lozymon.loadTPO();
 
     return Lozymon;
 
---
+++ Maker.commons/lm/tpo.json.js
@@ -0,0 +1,10 @@
+var lm.tpo = [
+    {
+        "sort":10,                  // object is sorted by this integer
+        "description":"jquery",     // add a description
+        "version":"2.1.1",          // object version
+        "type":"d",                 // J = Javascript or S = StyleSheet
+        "path":"path/to/object.js", // path to the objects
+        "useWebrunInclude": true    // if javascript use webrun.include or <script></script> tag
+    }
+];
